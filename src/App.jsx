70bea579f05a86491696bd354309d5aeebeddbad
yantedesco <yantedesco@gmail.com>
2022-10-28 20:03:26 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { pokemons } from "./data/pokemon.js"
+import { Pokemon } from "./screens/Pokemon"
+
+function App() {
+
+  return (
+    <Pokemon />
+  );
+}
+
+export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+export const Button = () => {
+  const estiloBotao = {
+    backgroundColor: 'red',
+    padding: '4px 12px',
+    fontSize: 12
+  }
+
+  return (
+    <button style={estiloBotao} onClick={() => null}>Botão</button>
+  )
+}
